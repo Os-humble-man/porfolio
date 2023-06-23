e7b5de23b8115651ba76e25856b8c0b83a77e01d
Os-humble-man <Oscarkanangila01@gmail.com>
2023-06-23 10:15:40 +0200

    add style and img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiSun, FiMoon } from "react-icons/fi";
+import { CgMenuRightAlt, CgClose } from "react-icons/cg";
+import logo from "./img/Red_Creative_Letter_K_Monogram_Logo__1_-removebg-preview.png";
 
 export default function Hero() {
+  const navLink = ["Accueil", "Apropos", "Compétences", "Contactes"];
+
   return (
-  <div className='header'>
-    {/* <Navbar/> */}
-    <div className="content">
-      <div className="left"></div>
-      <div className="right">
-        
+    <div className="full-container">
+      <div className="frame1">
+        <div className="header">
+          <div className="logo">
+            <div>Porfolio</div>
+          </div>
+          <div className="nav-link">
+            <ul>
+              {navLink.map((e, i) => (
+                <li key={i}>
+                  <Link to={e}>{e}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="dark-mode-toggle">
+            <FiSun className="icon" />
+            {/* <FiMoon className="icon"/> */}
+          </div>
+
+          <div className="hamburger-menu-btn">
+            <CgMenuRightAlt />
+            <CgClose />
+          </div>
+        </div>
+        <div class="left-side">{/* img */}</div>
+        <div class="right-side"></div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,31 +6,7 @@ import logo from "./img/Red_Creative_Letter_K_Monogram_Logo__1_-removebg-preview
 
 export default function Navbar() {
   // Nav items
-  const navLink = ["Accueil", "Apropos", "Compétences", "Contactes"];
   return (
-    <div className="nav">
-      <div className="logo">
-        <img src={logo} alt="logo" />
-      </div>
-      <div className="nav-link">
-        <ul>
-          {navLink.map((e, i) => (
-            <li key={i}>
-              <Link to={e}>{e}</Link>
-            </li>
-          ))}
-          
-          <div className="dark-mode-toggle">
-            <FiSun />
-            {/* <FiMoon /> */}
-          </div>
-        </ul>
-      </div>
-
-      <div className="hamburger-menu-btn">
-        <CgMenuRightAlt />
-        <CgClose />
-      </div>
-    </div>
+    
   );
 }
