cee4bcfba179adf1409624fe3e5a380fa3dedec8
Os-humble-man <Oscarkanangila01@gmail.com>
2023-07-10 17:48:46 +0200

    "start skills section"

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import About from "./components/About";
 import Hero from "./components/Hero";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Skills from "./components/Skills";
 
 function App() {
-  
+
   return (
     <Router>
       <div className="full-container">
         <div className="container">
           <Hero />
-          <About/>
+          <About />
+          <Skills />
         </div>
       </div>
     </Router>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,14 +3,20 @@ import React from 'react'
 export default function Skills() {
     return (
         <div className='frame frame4'>
-            <div className="title"></div>
-            <div className="content"></div>
             <div className="skills">
-                <div className="skill skill1"></div>
-                <div className="skill skill2"></div>
-                <div className="skill skill3"></div>
-                <div className="skill skill4"></div>
+                <div className="skill skill1">
+                    <div className="content-skill"></div>
+                </div>
+                <div className="skill skill2">
+                    <div className="content-skill"></div>
+                </div>
+                <div className="skill skill3">
+                    <div className="content-skill"></div>
+                </div>
+                <div className="skill skill4">
+                    <div className="content-skill"></div>
+                </div>
             </div>
-        </div>  
+        </div>
     )
 }
