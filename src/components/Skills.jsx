f80e3969ef22e6d18fb3b7f108323d95833392e1
Os-humble-man <Oscarkanangila01@gmail.com>
2023-07-18 15:13:56 +0200

    'end skill section '

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import { skills } from "./data";
 export default function Skills() {
-    return (
-        <div className='frame frame4'>
-            <div className="skills">
-                <div className="skill skill1">
-                    <div className="content-skill"></div>
-                </div>
-                <div className="skill skill2">
-                    <div className="content-skill"></div>
-                </div>
-                <div className="skill skill3">
-                    <div className="content-skill"></div>
-                </div>
-                <div className="skill skill4">
-                    <div className="content-skill"></div>
-                </div>
+  return (
+    <div className="frame frame4">
+      <div className="skills">
+        {skills.map((skill) => (
+          <div
+            key={skill.id}
+            style={{
+              backgroundImage: `linear-gradient(${skill.gradientColor.join(
+                ", "
+              )}),url(${skill.imgUrl})`,
+            }}
+            className="skill skill1"
+          >
+            <div className="content-skill">
+              <h1>{skill.name}</h1>
             </div>
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/components/data.js
@@ -0,0 +1,30 @@
+export const skills = [
+  {
+    id: 1,
+    name: "Developpeur Web",
+    imgUrl:
+      "https://img.freepik.com/vecteurs-libre/logo-code-degrade-creatif_23-2148820572.jpg?w=740&t=st=1689685034~exp=1689685634~hmac=7bfe574c068b56a3c6b62921f3c5a736fd31dbb67cc0609c555fa926942a2a98",
+    gradientColor: ["rgba(255, 168, 0, 0.85)", "rgba(255, 168, 0, 0.85)"],
+  },
+  {
+    id: 2,
+    name: "Designer UI|UX",
+    imgUrl:
+      "https://img.freepik.com/vecteurs-libre/illustration-concepteur-graphique_114360-338.jpg?w=740&t=st=1689675043~exp=1689675643~hmac=8be2cd3fc78f434564e011564b6605d58c19cb9e0cb97af1c3820656fd3e5ca4",
+    gradientColor: ["rgba(56, 217, 16, 0.86)", "rgba(56, 217, 16, 0.86)"],
+  },
+  {
+    id: 3,
+    name: "Photographe",
+    imgUrl:
+      "https://img.freepik.com/photos-gratuite/vue-face-femme-travaillant-comme-photographe_23-2150506121.jpg?w=740&t=st=1689682480~exp=1689683080~hmac=95d375fb51d0b318717e37d1b7487980f26182ddb1d739dcd21f9f0351f9e0df",
+    gradientColor: ["rgba(0, 148, 255, 0.79)", "rgba(0, 148, 255, 0.79)"],
+  },
+  {
+    id: 4,
+    name: "IT Support",
+    ImgUrl:
+      "https://img.freepik.com/vecteurs-libre/dans-illustration-du-concept-synchronisation_114360-1029.jpg?w=740&t=st=1689685535~exp=1689686135~hmac=645ff6b44c9d8073fbc2b9831f359771a693498cec5a40c8d9d047002cb9618a",
+    gradientColor: ["rgba(118, 3, 172, 0.87)", "rgba(118, 3, 172, 0.87)"],
+  },
+];
